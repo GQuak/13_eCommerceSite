97287648f93bceafbe8241808b248591fbc85840
GQuak <75180482+GQuak@users.noreply.github.com>
2021-05-19 21:21:28 -0600

    correcting routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -24,6 +24,24 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      // JOIN with Category and Tag
+      include: [
+        { model: Tag, through: ProductTag, as: "product_tags" },
+        { model: Category },
+      ],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: "No tag found with this id!" });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
